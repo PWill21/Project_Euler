81247cc3ed88dec0c02c007a8062657f3ee08dff
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-07 01:26:08 -0400

    change add() method to take a maximum Fibonacci Number value

--- ProjectEuler02.rb
+++ ProjectEuler02.rb
@@ -1,17 +1,23 @@
 def add (n)
 	first_number = 1
 	second_number = 2
+	fib_number = 0
 	puts first_number
 	puts second_number
-	(n-2).times do 
+	continue = true
+	while continue
 		fib_number = first_number + second_number
-		puts fib_number
-		first_number = second_number
-		second_number = fib_number
+		if fib_number < n
+			puts fib_number
+			first_number = second_number
+			second_number = fib_number
+		else
+			continue = false			
+		end
 	end
 end
 
-puts "How many Fibonacci Numbers would you like to find?"
-number_of_fibs = gets
+puts "What is the maximum Fibonacci value?"
+max_fig = gets
 puts
-add(number_of_fibs.to_i)
+add(max_fig.to_i)
