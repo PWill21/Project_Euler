ee556b254818333d0a4d4ba226943dc22d955001
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-11 01:40:56 -0400

    add code that finds all prime numbers up to a user-given number

--- ProjectEuler03.rb
+++ ProjectEuler03.rb
@@ -1,2 +1,9 @@
 puts "What number would you like to find the prime factor of?"
-number = gets
+number = gets.to_i
+require 'prime'
+number.times do |i|
+	if Prime.prime?(i)
+		puts i
+		i+1
+	end
+end
