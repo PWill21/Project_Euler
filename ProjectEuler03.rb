367ddc6987c80b39c50423bc063ada4420418d48
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-11 01:44:16 -0400

    add primes[] array to store all prime numbers

--- ProjectEuler03.rb
+++ ProjectEuler03.rb
@@ -1,9 +1,11 @@
 puts "What number would you like to find the prime factor of?"
 number = gets.to_i
+primes = []
 require 'prime'
 number.times do |i|
 	if Prime.prime?(i)
-		puts i
+		primes.push(i)
 		i+1
 	end
 end
+puts primes
